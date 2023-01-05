aa67e54b08efe37a95c6b5795e97eedc6a6f8f14
Didrick Manahan <didrick.manahan@gmail.com>
2023-01-04 21:42:33 -0500

    build out single page event

--- newsletter_app/pages/events/[cat]/[id].js
+++ newsletter_app/pages/events/[cat]/[id].js
@@ -1,7 +1,40 @@
-import { Mountains_of_Christmas } from "@next/font/google";
+import Image from "next/image";
 
-const EventPage = () => {
-  return <h1> Our single event </h1>;
+const EventPage = ({ data }) => {
+  return (
+    <div>
+      <Image src={data.image} width={500} height={300} alt={data.title} />
+      <h1> {data.title} </h1>
+      <p> {data.description} </p>
+    </div>
+  );
 };
 
 export default EventPage;
+
+export async function getStaticPaths() {
+  const { allEvents } = await import("/data/data.json");
+  const allPaths = allEvents.map((path) => {
+    return {
+      params: {
+        cat: path.city,
+        id: path.id,
+      },
+    };
+  });
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  // same as first two lines below getStaticPaths
+  const id = context.params.id;
+  const { allEvents } = await import("/data/data.json");
+  const eventData = allEvents.find((ev) => id === ev.id);
+
+  return {
+    props: { data: eventData },
+  };
+}
