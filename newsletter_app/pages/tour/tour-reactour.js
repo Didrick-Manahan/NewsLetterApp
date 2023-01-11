18a33a45b761badcb7dcbfefd71bb71b4c7918f8
Didrick Manahan <didrick.manahan@gmail.com>
2023-01-11 02:28:18 -0500

    tour page setup

---
+++ newsletter_app/pages/tour/tour-reactour.js
@@ -0,0 +1,21 @@
+const TourPageReactour = () => {
+  return (
+    <div>
+      <h1 className="tour-title"> Welcome to Our React Tour! </h1>
+
+      <h2> First Step </h2>
+      <p>
+        Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+        suscipit laboriosam,
+      </p>
+
+      <h2> Second Step </h2>
+      <p>
+        nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure
+        reprehenderit
+      </p>
+    </div>
+  );
+};
+
+export default TourPageReactour;
