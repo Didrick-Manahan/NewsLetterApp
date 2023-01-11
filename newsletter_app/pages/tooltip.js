e2df0fba1e986ce55cd4d9e14a2828f1ab5a4058
Didrick Manahan <didrick.manahan@gmail.com>
2023-01-11 02:20:03 -0500

    intial reactour exploration

--- newsletter_app/pages/tooltip.js
+++ newsletter_app/pages/tooltip.js
@@ -1,16 +1,39 @@
 import { Tooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
+//import Tour from "reactour";
+import { TourProvider } from "@reactour/tour";
+import { useTour } from "@reactour/tour";
 
-const ToolTipPage = () => {
+import Loadable from "react-loadable";
+
+//const Tour = Loadable({
+//  loader: () => import("reactour"),
+//  loading: () => null,
+//});
+
+const ToolTipPage = (props) => {
+  const { setIsOpen } = useTour();
   return (
     <div>
-      <h1>Testing ToolTip</h1>
+      <TourProvider steps={steps}>
+        <h1>Testing ToolTip</h1>
 
-      <p id="my-anchor-id">Tooltip</p>
+        <p id="my-anchor-id">Tooltip</p>
 
-      <Tooltip anchorId="my-anchor-id" content="hello world" place="bottom" />
+        <Tooltip anchorId="my-anchor-id" content="hello world" place="bottom" />
+      </TourProvider>
+
+      <button onClick={() => setIsOpen(true)}>Open Tour</button>
     </div>
   );
 };
+
+const steps = [
+  {
+    selector: ".first-step",
+    content: "This is my first Step",
+  },
+  // ...
+];
 
 export default ToolTipPage;
